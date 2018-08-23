7ad939b9d27048feb0fde3059a7acca4fe108d63
Vivien Richter <vivien-richter@outlook.de>
2018-08-23 23:41:11 +0200

    Sample config file added

---
+++ application/config.php
@@ -0,0 +1,19 @@
+<?php
+return array(
+'debug' => FALSE,
+'webmaster' => 'Vivien Richter',
+'webmaster_email' => 'webmaster@slsr.org',
+'project_name' => 'SL Shrine list',
+
+'database_host' => 'localhost',
+'database_port' => 3306,
+'database_db' => 'slsr',
+'database_charset' => 'utf8',
+'database_user' => 'root',
+'database_password' => '4321',
+
+'default_client_page' => 'random',
+'default_web_page' => 'about',
+'charset' => 'utf-8'
+);
+?>
