c41d13466bf9631bcca8698dc2c1088aa93a402f
Vivien Richter <vivien-richter@outlook.de>
2018-09-05 16:32:19 +0200

    Update placeholder page added

---
+++ public/update_placeholder_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+  <meta name="msapplication-TileColor" content="#ffffff"/>
+  <meta name="msapplication-TileImage" content="/mstile-144x144.png"/>
+  <meta name="author" content="Vivien Richter"/>
+  <meta name="description" content="The Second Life Shinto Shrine Register (SLSR) is a association of Shinto shrines in Second Life."/>
+  <meta name="keywords" content="Second Life, SL, Shinto, shrine register, board"/>
+  <title>Second Life Shinto Shrine Register</title>
+  <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico"/>
+  <link rel="icon" type="image/png" href="/favicon.png" sizes="32x32"/>
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+ </head>
+ <body>
+  <main>
+   	<h1>Update in progress..</h1>
+   	<p>We will be back in a few minutes.</p>
+   	<p>If you have any questions, please feel free to <a href="mailto:webmaster@slsr.org">contact</a> us.</p>
+  </main>
+ </body>
+</html>
