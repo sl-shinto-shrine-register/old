0fd8cdb605b480573777d0ec902d097555e92ac0
Vivien Richter <vivien-richter@outlook.de>
2019-09-28 18:52:17 +0200

    Database structure improved and foundation date for the shrines introduced.

--- database/structure/articles.sql
+++ database/structure/articles.sql
@@ -1,51 +1,18 @@
--- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
---
--- Host: localhost    Database: slsr_old
--- ------------------------------------------------------
--- Server version	5.7.27-0ubuntu0.18.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `articles`
---
-
 DROP TABLE IF EXISTS `articles`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `articles` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `caption_en` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description_en` text COLLATE utf8mb4_unicode_ci,
-  `link` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `owner_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `caption_de` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `caption_ja` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `description_de` text COLLATE utf8mb4_unicode_ci,
-  `description_ja` text COLLATE utf8mb4_unicode_ci,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `id` int(11) NOT NULL AUTO_INCREMENT COMMENT 'Incrementing ID',
+  `caption_en` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name (english)',
+  `caption_ja` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name (japanese)',
+  `caption_de` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name (german)',
+  `description_en` text COLLATE utf8mb4_unicode_ci COMMENT 'Description (english)',
+  `description_ja` text COLLATE utf8mb4_unicode_ci COMMENT 'Description (japanese)',
+  `description_de` text COLLATE utf8mb4_unicode_ci COMMENT 'Description (german)',
+  `link` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Link',
+  `founded_at` timestamp NULL DEFAULT NULL COMMENT 'Foundation date',
+  `owner_id` varchar(36) COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Owner',
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Creation timestamp',
+  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Update timestamp',
   PRIMARY KEY (`id`),
   KEY `owner_id` (`owner_id`),
   CONSTRAINT `owner` FOREIGN KEY (`owner_id`) REFERENCES `owners` (`id`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=72 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-04  4:46:00
+) ENGINE=InnoDB AUTO_INCREMENT=73 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
--- database/structure/articles_to_pages.sql
+++ database/structure/articles_to_pages.sql
@@ -1,27 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.27, for Linux (x86_64)
---
--- Host: localhost    Database: slsr_old
--- ------------------------------------------------------
--- Server version	5.7.27-0ubuntu0.18.04.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `articles_to_pages`
---
-
 DROP TABLE IF EXISTS `articles_to_pages`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `articles_to_pages` (
   `page_id` int(11) NOT NULL,
   `article_id` int(11) NOT NULL,
@@ -30,15 +7,3 @@ CREATE TABLE `articles_to_pages` (
   CONSTRAINT `article` FOREIGN KEY (`article_id`) REFERENCES `articles` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `page` FOREIGN KEY (`page_id`) REFERENCES `pages` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2019-09-04  4:46:00
